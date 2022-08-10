e0f8fcaa8347ad4ba69cf046f7456f21714b5c4c
brokiem <milyhqvideo@gmail.com>
2022-08-10 13:53:39 +0800

    fix mapping protocol [release]

--- src/ColinHDev/ActualAntiXRay/tasks/ChunkRequestTask.php
+++ src/ColinHDev/ActualAntiXRay/tasks/ChunkRequestTask.php
@@ -167,7 +167,7 @@ class ChunkRequestTask extends PMMPChunkRequestTask {
         assert($chunk instanceof Chunk);
         $subCount = ChunkSerializer::getSubChunkCount($chunk) + ChunkSerializer::LOWER_PADDING_SIZE;
         $encoderContext = new PacketSerializerContext(GlobalItemTypeDictionary::getInstance()->getDictionary(GlobalItemTypeDictionary::getDictionaryProtocol($this->mappingProtocol)));
-        $payload = ChunkSerializer::serializeFullChunk($chunk, RuntimeBlockMapping::getInstance(), $encoderContext, $this->tiles);
+        $payload = ChunkSerializer::serializeFullChunk($chunk, RuntimeBlockMapping::getInstance(), $encoderContext, $this->mappingProtocol, $this->tiles);
         $this->setResult($this->compressor->compress(PacketBatch::fromPackets($this->mappingProtocol, $encoderContext, LevelChunkPacket::create($this->chunkX, $this->chunkZ, $subCount, false, null, $payload))->getBuffer()));
     }
 
